7b69152961facb6cf34570a860e060534d3c0068
fds18 <47363809+fds18@users.noreply.github.com>
2019-02-05 17:44:40 +0000

    Initial commit

---
+++ Coding.ino
@@ -0,0 +1,57 @@
+//included libraries
+
+
+//function declarations
+bool mag_up ();
+bool mag_down ();
+bool radio ();
+bool infra ();
+bool sound ();
+
+void commands ();
+
+int analysis ();
+
+void setup() {
+  // put your setup code here, to run once:
+  bool start = true;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  //Working code starts here and runs in an infinite loop
+  while (start){
+    
+  }
+  
+}
+
+bool mag_up (){
+  
+}
+
+bool mag_down(){
+  
+}
+
+bool radio (){
+  
+}
+
+bool infra (){
+  
+}
+
+bool sound (){
+
+}
+
+void commands () {
+  
+}
+
+int analysis (bool mag_up, bool mag_down, bool radio, bool infra, bool sound){
+  
+}
